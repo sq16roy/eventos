e14d128f37f994a54e24316aa0e6cbf75d2318bc
roy.solera <roy.solera@rivetlogic.com>
2019-11-29 08:53:42 -0600

    new app

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-
-// Se utiliza http dentro de la arquitectura
-const http = require('http');
-
-const port = 3000; //Se establece el puerto 3000 como punto de origen para acceder a la aplicación, para acceder se usa localhost:3000 en el navegador
-
-const serveStatic = require('serve-static'); //Es la dependencia que permite crear un servidor estático
-// Se exporta la conexión de nodejs
-const connect = require('connect');
-
 const nodemon = require('nodemon');
+const path = require('path');
 
-
-connect().use(serveStatic(__dirname)).listen(port, () => {
-  console.log(`La aplicación esta levantada dentro del puerto ${port} ingrese usando localhost:3000/public`);
-  nodemon({
-    script: 'api/index.js',
-    ext: 'js'
-  });
+nodemon({
+  execMap: {
+    js: 'node'
+  },
+  script: path.join(__dirname, 'server/server'),
+  ignore: [],
+  watch: process.env.NODE_ENV !== 'production' ? ['server/*'] : false,
+  ext: 'js'
+})
+.on('restart', function() {
+  console.log('Server restarted!');
+})
+.once('exit', function () {
+  console.log('Shutting down server');
+  process.exit();
 });
