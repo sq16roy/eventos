1897a19a3011567c02dcc4f8c25b836ff5e87fe5
mauro <jsoleraq@ucenfotec.ac.cr>
2019-12-08 23:58:13 -0600

    changes

--- client/src/components/CreateLugar.jsx
+++ client/src/components/CreateLugar.jsx
@@ -7,6 +7,7 @@ class CreateLugar extends Component {
         super(props);
         this.state = {
             nombre: '',
+            cupo: '',
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,25 +21,64 @@ class CreateLugar extends Component {
     handleSubmit(e) {
         const {
             nombre,
+            cupo,
+            provincia,
+            direccion,
         } = this.state;
 
         e.preventDefault();
         this.props.createEvento({
             nombre,
+            cupo,
+            provincia,
+            direccion,
         });
         this.setState({
             nombre: '',
+            cupo: '',
+            direccion: '',
+
         });
     }
 
     render() {
         const {
-           nombre,
+            nombre,
+            cupo,
+            provincia,
+            direccion,
         } = this.state;
 
         return (
             <div className="crete_event_form_container">
                 <form className="crete_event_form" onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input value={nombre} onChange={this.handleChange} type="text" name="nombre" required />
+                    </div>
+
+                    <div className="inputBox">
+                        <label htmlFor="sltDireccion">Dirección<span className="required"> *</span></label>
+                        <select className="tipo_slt" id="provincia" value={provincia} name='provincia' required onChange={this.handleChange}>
+                            <option value="">Provincia</option>
+                            <option value="San Jose">San José</option>
+                            <option value="Alajuela">Alajuela</option>
+                            <option value="Cartago">Cartago</option>
+                            <option value="Heredia">Heredia</option>
+                            <option value="Guanacaste">Guanacaste</option>
+                            <option value="Puntarenas">Puntarenas</option>
+                            <option value="Limon">Limón</option>
+                        </select>
+                    </div>
+                    <div className="inputBox">
+                        <label htmlFor="txtDireccion">Dirección exacta<span className="required"> *</span></label>
+                        <input className="txtDireccion" id="txtDireccion" required value={direccion} name='direccion' onChange={this.handleChange}></input>
+                    </div>
+
+                    <div>
+                        <label htmlFor="cantidadAsistentes">Cupo</label>
+                        <input value={cupo} onChange={this.handleChange} type="number" name="cupo" />
+                    </div>
                     <button className="submit_btn" type="submit">Crear</button>
                 </form>
             </div>
