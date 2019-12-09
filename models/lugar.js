af9c935b9e08e576a4dbfda2faf14849c6d7919d
mauro <jsoleraq@ucenfotec.ac.cr>
2019-12-09 00:00:50 -0600

    changes

--- models/lugar.js
+++ models/lugar.js
@@ -9,22 +9,22 @@ const lugarSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now
 	},
-    provincia:{
+	provincia: {
 		type: String,
 		required: true
 	},
-    direcion:{
-        type: String,
-        required: true
+	direccion: {
+		type: String,
+		required: true
 	},
-    cupo:{
+	cupo: {
 		type: Number,
 		required: true
 	},
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	}
 });
 
 module.exports = mongoose.model('Lugar', lugarSchema);
